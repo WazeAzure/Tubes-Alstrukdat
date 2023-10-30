b141a9452f1987eb3e6dd46ad0949a19b5911bc7
Melati Anggraini <112107340+mlatia@users.noreply.github.com>
2023-10-30 20:35:01 +0700

    utas

--- ADT/Utas/utas.h
+++ ADT/Utas/utas.h
@@ -10,29 +10,38 @@ typedef struct {
     int index; // index dari 1,...
     Word utasan;
     DATETIME time;
+    MAINUTAS parent;
 } UTAS;
 
 typedef struct {
-    int IDKicauan;
-    int IDUtas; 
+    int idkicau;
+    int idutas; 
     Node child; 
 } MAINUTAS;
 
-void CreateMainUtas(MAINUTAS *parent, int IDKicauan, int IDUtas, Node child);
+// SELEKTOR //
+// UTAS
+#define INDEX(u) (u).index
+#define INPUT(u) (u).utasan
+#define TIME(u) (u).time
 
-void CetakMainUtas(MAINUTAS *parent);
+// MAINUTAS
+#define ID_KICAUAN(m) (m).idkicau
+#define ID_UTAS(m) (m).idutas
 
-void CreateUtas(UTAS *utas, int index, Word utasan, DATETIME time);
+void CreateMainUtas(MAINUTAS *parent, int idkicau, int idutas, Node child);
 
-void ReadUtas(UTAS utas);
+void CetakMainUtas(MAINUTAS parent);
 
-void CetakUtas(UTAS *utas);
+void CreateUtas(UTAS *utas, int index, Word utasan, DATETIME time, MAINUTAS parent);
 
-void SambungUtas(int IDUtas,int index, UTAS *utas);
+void ReadUtas(UTAS *utas);
 
-void HapusUtas(int IDUtas,int index, UTAS *utas); 
+void CetakUtas(UTAS utas);
 
+void SambungUtas(int idutas,int index, UTAS *utas);
 
+void HapusUtas(int idutas,int index, UTAS *utas); 
 
 
 #endif
