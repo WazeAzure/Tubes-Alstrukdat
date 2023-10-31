17f5983bd15010f4aeade73509548e09392ce27f
Melati Anggraini <112107340+mlatia@users.noreply.github.com>
2023-10-31 13:52:30 +0700

    utas1

---
+++ ADT/Utas/utas.c
@@ -0,0 +1,21 @@
+#include "utas.h"
+
+void CreateUtas(UTAS *utas, int index, Word utasan, DATETIME time, int idutas){
+
+}
+
+void ReadUtas(UTAS *utas){
+
+}
+
+void CetakUtas(UTAS utas){
+
+}
+// search apakah ada 
+void SambungUtas(int idutas,int index, UTAS *utas){
+
+}
+
+void HapusUtas(int idutas,int index, UTAS *utas){
+    
+}
