4315229f0e0280b6453a142845cbee17eaec2389
WazeAzure <bryan.rich0604@gmail.com>
2023-10-31 22:34:12 +0700

    Update: lupa nge save main.c keburu di push tadi :v

--- main.c
+++ main.c
@@ -67,72 +67,95 @@ int main(){
                 printExitBanner();
             }
             else if(strCompare(input[0].TabWord, command[4])){
+                // GANTI_PROFIL
                 printf("called %s\n", command[4]);
             }
             else if(strCompare(input[0].TabWord, command[5])){
+                // LIHAT_PROFIL
                 printf("called %s\n", command[5]);
             }
             else if(strCompare(input[0].TabWord, command[6])){
+                // ATUR_JENIS_AKUN
                 printf("called %s\n", command[6]);
             }
             else if(strCompare(input[0].TabWord, command[7])){
+                // UBAH_FOTO_PROFIL
                 printf("called %s\n", command[7]);
             }
             else if(strCompare(input[0].TabWord, command[8])){
+                // DAFTAR_TEMAN
                 printf("called %s\n", command[8]);
             }
             else if(strCompare(input[0].TabWord, command[9])){
+                // HAPUS_TEMAN
                 printf("called %s\n", command[9]);
             }
             else if(strCompare(input[0].TabWord, command[10])){
+                // TAMBAH_TEMAN
                 printf("called %s\n", command[10]);
             }
             else if(strCompare(input[0].TabWord, command[11])){
+                // BATAL_TAMBAH_TEMAN
                 printf("called %s\n", command[11]);
             }
             else if(strCompare(input[0].TabWord, command[12])){
+                // DAFTAR_PERMINTAAN_PERTEMANAN
                 printf("called %s\n", command[12]);
             }
             else if(strCompare(input[0].TabWord, command[13])){
+                // SETUJUI PERTEMANAN
                 printf("called %s\n", command[13]);
             }
             else if(strCompare(input[0].TabWord, command[14])){
+                // KICAU
                 printf("called %s\n", command[14]);
             }
             else if(strCompare(input[0].TabWord, command[15])){
+                // KICAUAN
                 printf("called %s\n", command[15]);
             }
             else if(strCompare(input[0].TabWord, command[16])){
+                // SUKA_KICAUAN
                 printf("called %s\n", command[16]);
             }
             else if(strCompare(input[0].TabWord, command[17])){
+                // UBAH_KICAUAN
                 printf("called %s\n", command[17]);
             }
             else if(strCompare(input[0].TabWord, command[18])){
+                // BALAS
                 printf("called %s\n", command[18]);
             }
             else if(strCompare(input[0].TabWord, command[19])){
+                // BALASAN
                 printf("called %s\n", command[19]);
             }
             else if(strCompare(input[0].TabWord, command[20])){
+                // HAPUS_BALASAN
                 printf("called %s\n", command[20]);
             }
             else if(strCompare(input[0].TabWord, command[21])){
+                // BUAT_DRAF
                 printf("called %s\n", command[21]);
             }
             else if(strCompare(input[0].TabWord, command[22])){
+                // LIHAT_DRAF
                 printf("called %s\n", command[22]);
             }
             else if(strCompare(input[0].TabWord, command[23])){
+                // UTAS
                 printf("called %s\n", command[23]);
             }
             else if(strCompare(input[0].TabWord, command[24])){
+                // SAMBUNG_UTAS
                 printf("called %s\n", command[24]);
             }
             else if(strCompare(input[0].TabWord, command[25])){
+                // HAPUS_UTAS
                 printf("called %s\n", command[25]);
             }
             else if(strCompare(input[0].TabWord, command[26])){
+                // CETAK UTAS
                 printf("called %s\n", command[26]);
             }
             else if(strCompare(input[0].TabWord, command[27])){
