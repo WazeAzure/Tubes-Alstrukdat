a93c35433ad8bb1a69cf618f366bef89407c0b15
hilalhmdy <mhdhilalhmdy15@gmail.com>
2023-11-20 17:24:03 +0700

    add balasan

--- ADT/Balasan/balasan.h
+++ ADT/Balasan/balasan.h
@@ -1,77 +1,24 @@
-// #ifndef _BALASAN_h
-// #define _BALASAN_h
-
-// #include "../Wordmachine/wordmachine.h"
-// #include "../Datetime/datetime.h"
-
-// typedef struct 
-// {
-//     int id;
-//     Word isi;
-//     Word author;
-//     int idAuthor;
-//     DATETIME date;
-
-// } BALASAN;
-
-// /*--------- SELEKTOR--------*/
-// #define idBalasan(x) (x).id
-// #define isiBalasan(x) (x).isi
-// #define authorBalasan(x) (x).author
-// #define authorBalasan2(x) (x).idAuthor
-// #define dateBalasan(x) (x).date
-
-// #endif
-
 #ifndef _BALASAN_h
 #define _BALASAN_h
-
-#include "../boolean.h"
 
 #include "../Wordmachine/wordmachine.h"
 #include "../Datetime/datetime.h"
 
-#include "../Balasan/balasan.h"
-#include "../Utas/utas.h"
-
-typedef struct {
+typedef struct 
+{
     int id;
-    BALASAN *list_balasan;
-} PairBalasan;
-
-typedef struct {
-    int idParent;
-    int id;
-    int like;
+    Word isi;
+    Word author;
     int idAuthor;
-    int inc_balasan;
-    Word teks;
-    Word Author;
-    DATETIME timeCreated;
+    DATETIME date;
 
-    // list of daftar balasan
-    PairBalasan *daftar_balasan;
-
-    // list of daftar utas
-    UTAS *daftar_utas;
 } BALASAN;
 
-typedef struct {
-    BALASAN* buffer;
-    int NEFF;
-    int CAPACITY;
-} LISTBALASAN;
-
-#define FIRST(l) (l)
-
-void CreateListBalasan(LISTBALASAN* l, int CAPACITY);
-
-// void CreateDaftarBalasan(BALASAN* BALASAN);
-
-void CreateBalasan(BALASAN* balasan, int idParent, int idAuthor, Word Author, Word teks);
-
-void balasan(Word Author);
-
-void showBalasanContent(BALASAN balasan);
+/*--------- SELEKTOR--------*/
+#define idBalasan(x) (x).id
+#define isiBalasan(x) (x).isi
+#define authorBalasan(x) (x).author
+#define idAuthorBalasan(x) (x).idAuthor
+#define dateBalasan(x) (x).date
 
 #endif
